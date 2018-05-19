dc9676c83ad87df25fcf7dbbc5b13cd0ae657dd2
Marcin Micek <marcin.micek@gmail.com>
2018-05-19 14:31:46 +0200

    First working version of web2-vue.js project

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    
+    <div v-if="!isAuthenticated">
+      <label>Zaloguj się emailem</label>
+      <input type="email" v-model="email">
+      <button @click="logIn()">Wchodzę</button>
+    </div>
+
+    <div v-else>
+      <h2>Witaj {{ email }}!</h2>
+      <b>
+        <p>
+          <a href @click="logOut()">Wyloguj</a>
+        </p>
+      </b>
+    </div>
+
   </div>
 </template>
 
@@ -24,37 +25,23 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hello World!!!'
+      email: 'marcin.micek@gmail.com',
+      isAuthenticated: false
+    }
+  },
+  methods: {
+    logIn() {
+      this.isAuthenticated = true;
+    },
+    logOut() {
+      this.isAuthenticated = false;
     }
   }
 }
+
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
